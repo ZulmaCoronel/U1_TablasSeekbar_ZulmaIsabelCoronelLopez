ef8fd7045ac62fe2196f75d449afa9498f3408cb
Zulma <zuiscoronello@ittepic.edu.mx>
2018-02-12 13:50:56 -0700

    actualizadoo

--- app/src/main/java/mx/edu/ittepic/u1_tablasseekbar_zulmaisabelcoronellopez/MainActivity.java
+++ app/src/main/java/mx/edu/ittepic/u1_tablasseekbar_zulmaisabelcoronellopez/MainActivity.java
@@ -1,76 +1,66 @@
 package mx.edu.ittepic.u1_tablasseekbar_zulmaisabelcoronellopez;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    SeekBar valorM;
-    EditText valorN;
-    TextView ResTabla,progreso;
-    int num1 = 0,num2 = 0, tot = 0;
-    String res = "";
-    ListView lvTablas;
-    ArrayList<String> al;
+    Button botoncal;
+    SeekBar seekBar;
+    TextView avance;
+    String concat,total;
+    int selec,respuesta;
+    ListView lista;
+    String [] array = new String[21];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        valorN = (EditText) findViewById(R.id.etNumero1);
-        valorM = (SeekBar) findViewById(R.id.seekBar);
-        ResTabla = (TextView) findViewById(R.id.tvResultado);
-        progreso = (TextView) findViewById(R.id.tvNumero2);
-        lvTablas = (ListView) findViewById(R.id.lvTablas);
-
+        seekBar = findViewById(R.id.seekBar);
+        avance = findViewById(R.id.tvResultado);
+        botoncal = findViewById(R.id.btncalcular);
+        lista = findViewById(R.id.lvTablas);
+        total="";
+        concat="";
+        selec=0;
         //-----------------------ListView---------------------------
-        al = new ArrayList<String>();
-        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, al);
-        lvTablas.setAdapter(aa);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,array);
 
-        //al = ArrayList     //aa ArrayAdapter
         //---------------------SeekBar--------------------------------
-        valorM.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                num2 = progress;
-                progreso.setText("Calcular hasta: "+num2);
-
-                res = "";
-                num1  = Integer.parseInt(valorN.getText().toString());
-
-                for(int i = 0 ; i <= 20;  i++){
-
-                    tot = num1 * i;
-                    res += num1 + " x "+ i + " = " + tot + "\n";
+                total="";
+                concat="";
+                avance.setText(""+progress);
+                selec = progress;
                 }
-
-                ResTabla.setText(res);
-                al.add(ResTabla.getText().toString());
-                lvTablas.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                        //int noItem = position;
-                        String valItem = (String)lvTablas.getItemAtPosition(position);
-                        Toast.makeText(getApplicationContext(), "Ha presionado: "+valItem, Toast.LENGTH_LONG).show();
-                    }
-                });
-            }
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
             }
-
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
             }
         });
-        }
+        botoncal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i=0;i<=20;i++){
+                    respuesta=selec*i;
+                    array[i]=""+selec+" X "+i+" = "+respuesta;
+                }
+                lista.setAdapter(adapter);
+            }
+        });
+    }
 }
+
+
